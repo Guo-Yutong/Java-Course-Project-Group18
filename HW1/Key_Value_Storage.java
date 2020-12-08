5ac73a92a77b5c7e5b6c38afcc8c719524e26193
TTa77 <guoyutong1022@outlook.com>
2020-12-08 22:41:11 +0800

    update - second commit

--- HW1/Key_Value_Storage.java
+++ HW1/Key_Value_Storage.java
@@ -13,7 +13,9 @@ public class Key_Value_Storage {
 	 * 支持给定Value，向存储中添加对应的Key-Value
 	 * 给定Key查找对应的Value
 	 */
-	private static FileInputStream readFile(String filepath) {	//读文件流
+	
+	/*
+	private static FileInputStream readFile(String filepath) {	//读文件流已嵌入add()方法中
 		try {
 			File file = new File(filepath);
 			FileInputStream is = new FileInputStream(file);
@@ -23,7 +25,8 @@ public class Key_Value_Storage {
 			return null;
 		}
 	}
-	
+	*/
+
 	/*
 	private static byte[] SHA1Checksum(InputStream is) throws Exception{	//SHA1算法，已嵌入add()方法中
 		byte[] buffer = new byte[1024];	//缓存器
@@ -40,18 +43,19 @@ public class Key_Value_Storage {
 	}
 	*/
 	
-	public static void add() {
-		File dir = new File("/home/yutong_guo/mygit");
-		if (!dir.exists()) {	//创建目录
+	public static void add(String filepath) {
+		File file = new File(filepath);	//需要被add的文件
+		String parentPath = file.getParent();	//获取该文件上级目录
+		
+		//若文件上级目录内没有git仓库则创建之
+		File dir = new File(parentPath + File.separator + "mygit");
+		if (!dir.exists()) {
 			dir.mkdir();
 		}
-		System.out.print("请输入文件路径: ");
-		Scanner input = new Scanner(System.in);
-		String filepath = input.nextLine();
-		FileInputStream is = readFile(filepath);	//创建输入流
 		
 		try {
-			FileOutputStream os = new FileOutputStream(dir+"/temp");	//创建输出流
+			FileInputStream is = new FileInputStream(filepath);	//创建输入流
+			FileOutputStream os = new FileOutputStream(dir+ File.separator + "temp");	//在仓库内创建一个名为temp的输出流，后续重命名为SHA1值
 			byte[] buffer = new byte[1];	//缓存器，只能一个一个字节读，否则填不满会使用末尾字节补齐，改变源文件
 											//但一个一个字节读会造成读取速度变慢，待解决
 			MessageDigest complete = MessageDigest.getInstance("SHA-1");
@@ -75,15 +79,15 @@ public class Key_Value_Storage {
 				result += Integer.toString(sha1[i]&0xFF,16);
 			}
 			//System.out.println(result);
-			File output = new File(dir+"/temp");
-			File output_new = new File(dir+"/"+result);
+			File output = new File(dir + File.separator + "temp");
+			File output_new = new File(dir + File.separator + result);
 			output.renameTo(output_new);	//将文件重命名为其内容的SHA1值
 		}catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
 	public static void main(String arg[]) throws FileNotFoundException {	//测试，可以产生在/mygit目录内以SHA1为文件名、文件内容不变的文件
-		add();
+		add("/home/yutong_guo/Software_Eng/class2.txt");
 		System.out.println("程序结束");
 	}
 
